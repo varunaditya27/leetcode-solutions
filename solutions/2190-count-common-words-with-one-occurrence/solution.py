c22afeb426b4c308a7f07a0e8c9330140b9676c9
QuantumCipherX <varun.paparajugari@gmail.com>
2024-09-16 04:46:35 +0000

    [LeetCode Sync] Runtime - 177 ms (5.34%), Memory - 17 MB (100.00%)

---
+++ solutions/2190-count-common-words-with-one-occurrence/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        count = 0
+        for word in words1:
+            if words1.count(word) == words2.count(word) == 1:
+                count += 1
+        
+        return count
