ee00dd2218bb4ef7609a76e181cdcfc46ca8773f
QuantumCipherX <varun.paparajugari@gmail.com>
2024-11-18 02:18:37 +0000

    [LeetCode Sync] Runtime - 340 ms (10.84%), Memory - 22.1 MB (100.00%)

---
+++ solutions/0344-reverse-string/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)):
+            s.insert(i, s.pop())
