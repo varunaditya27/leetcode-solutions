078fbb7fd70f1a71839cfed2979d9aeea57c2f48
QuantumCipherX <varun.paparajugari@gmail.com>
2026-01-12 14:22:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.1 MB (16.67%)

---
+++ solutions/0145-binary-tree-postorder-traversal/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helperFunc(TreeNode* root, vector<int>& traversal) {
+        if (root == nullptr) {
+            return;
+        }
+
+        helperFunc(root->left, traversal);
+        helperFunc(root->right, traversal);
+        traversal.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        helperFunc(root, result);
+
+        return result;
+    }
+};
