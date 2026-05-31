80d945b8313d1b112e3a7d515d372c5471fa282f
varunaditya27 <139733432+varunaditya27@users.noreply.github.com>
2026-05-31 02:31:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (64.77%)

---
+++ solutions/4321-digit-frequency-score/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int res = 0;
+        while (n > 0) {
+            res += n % 10;
+            n /= 10;
+        }
+
+        return res;
+    }
+};
